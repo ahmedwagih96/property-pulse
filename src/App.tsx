5e60e20d14cf696d022cd33afa4e31f4ff4d71ac
Ahmed Wagih <ahmd.wagih96@gmail.com>
2023-10-08 16:48:24 +0300

    created pages and routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Home, About, Profile, SignIn, SignUp } from "./pages";
 function App() {
-  return <div className="text-red-500">App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
