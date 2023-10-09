03c43add173a193e1e1e6ba677c1c75e37c21846
Ahmed Wagih <ahmd.wagih96@gmail.com>
2023-10-10 02:20:22 +0300

    added redux persist

--- client/src/redux/store.ts
+++ client/src/redux/store.ts
@@ -1,12 +1,23 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import userReducer from "./features/userSlice";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/es/storage";
+import persistStore from "redux-persist/es/persistStore";
+
+const rootReducer = combineReducers({ user: userReducer });
+const persistConfig = {
+  key: "root",
+  storage,
+  version: 1,
+};
+const reducers = persistReducer(persistConfig, rootReducer);
 export const store = configureStore({
-  reducer: {
-    user: userReducer,
-  },
+  reducer: reducers,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({ serializableCheck: false }),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
+
+export const persistor = persistStore(store);
