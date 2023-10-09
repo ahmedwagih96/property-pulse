dd08dbbe960ef1898cf7b69b94cad9af1332d5a7
Ahmed Wagih <ahmd.wagih96@gmail.com>
2023-10-09 15:36:00 +0300

    connected to DB

--- api/index.js
+++ api/index.js
@@ -1,7 +1,21 @@
-import express from 'express'
-
+const express = require('express')
 const app = express()
+const connectDB = require('./db/connect')
+require('dotenv').config();
 
-app.listen(3000, ()=>{
-    console.log(`Server is running on port 3000`)
-})
+
+// Running The Server
+const PORT = process.env.PORT || 3000
+
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URI);
+        app.listen(PORT, () =>
+            console.log(`Server is listening on port ${PORT}...`)
+        );
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+start();
