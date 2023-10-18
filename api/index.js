4175149c544d9f0c46de10b6de805e3dda286749
Ahmed Wagih <ahmd.wagih96@gmail.com>
2023-10-19 00:35:04 +0300

    fixed the static path to index.html file

--- api/index.js
+++ api/index.js
@@ -2,7 +2,6 @@ require('dotenv').config();
 require("express-async-errors");
 const connectDB = require('./db/connect')
 const xss = require('xss-clean')
-const rateLimiting = require('express-rate-limit');
 const helmet = require('helmet')
 const hpp = require('hpp');
 const { errorHandler } = require('./middleware/error');
@@ -16,42 +15,33 @@ app.use(cookieParser());
 
 // Security Headers (helmet)
 app.use(helmet());
-
 // Prevent XSS(Cross Site Scripting) Attacks
 app.use(xss());
-
 // Protect Http Param Pollution
 app.use(hpp());
 
-// Rate Limiting 
-app.set('trust proxy', true);
-app.use(rateLimiting({
-    windowMs: 10 * 60 * 1000, // 10 minutes
-    max: 200,
-}))
 // Routes
 app.use('/api/user', require("./routes/user.route.js"))
 app.use('/api/auth', require('./routes/auth.route.js'))
 app.use('/api/property', require('./routes/property.route.js'))
 
-const staticPath = path.join(__dirname, "..", '/client/dist');
-app.use(express.static(path.join(staticPath, '/client/dist')));
+const staticPath = path.join(__dirname, '../client/dist');
+app.use(express.static(staticPath));
 
 // Error Handler Middleware
 app.use(errorHandler);
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(staticPath, 'client', 'dist', 'index.html'));
+    res.sendFile(path.join(staticPath, 'index.html'));
 })
 
-// Running The Server
-const PORT = 8000
+console.log(path.join(staticPath, 'index.html'))
 
 const start = async () => {
     try {
         await connectDB(process.env.MONGO_URI);
-        app.listen(PORT, () =>
-            console.log(`Server is listening on port ${PORT}...`)
+        app.listen(8000, () =>
+            console.log(`Server is listening on port 8000...`)
         );
     } catch (error) {
         console.log(error);
