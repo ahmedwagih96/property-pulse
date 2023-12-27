44a0fb550249bc9922ee8870165e2b58994aef60
Ahmed Wagih <ahmd.wagih96@gmail.com>
2023-12-27 21:26:24 +0200

    revert: Revert accidental file copy

--- client/src/hooks/useProfileActions.tsx
+++ client/src/hooks/useProfileActions.tsx
@@ -1,139 +1,66 @@
-import { useState, FormEvent, ChangeEvent, SetStateAction } from "react";
-import { useNavigate } from "react-router-dom";
-import { ListingDataForm } from "../types/typings";
-// utils
-import { uploadImage } from "../utils/uploadImage";
-import {
-  updateFileList,
-  addFileToList,
-  sortFileList,
-} from "../utils/updateFileList";
-import { handleFormInputs } from "../utils/handleFormInputs";
-import { initialListingDataForm } from "../constants";
-import { validateCreateListingForm } from "../utils/formValidations";
-// Redux
-import { useAppDispatch } from "../redux/hooks";
-import { setUser } from "../redux/features/userSlice";
-import { DropResult } from "react-beautiful-dnd";
+import { useState } from "react";
+import { useAppDispatch, useAppSelector } from "../redux/hooks";
+import { signOut } from "../redux/features/userSlice";
+import { toast } from "react-toastify";
 import useAuthentication from "./useAuthentication";
-function useCreateListing() {
-  const { refreshToken } = useAuthentication();
+function useProfileActions() {
   const dispatch = useAppDispatch();
-  const navigate = useNavigate();
-  // state
-  const [fileUploadError, setFileUploadError] = useState<string>("");
+  const { currentUser } = useAppSelector((state) => state.user);
+  const { refreshToken } = useAuthentication();
   const [error, setError] = useState<string>("");
-  const [formData, setFormData] = useState<ListingDataForm>(
-    initialListingDataForm
-  );
-  const [loading, setLoading] = useState<boolean>(false);
-  const [files, setFiles] = useState<FileList>();
-
-  // remove selected files
-  const removeFile = (fileToRemove: File) => {
-    if (!files) {
-      return;
-    }
-    setFiles(updateFileList(files, fileToRemove));
-  };
-
-  const addFiles = (filesToAdd: FileList) => {
-    setFiles(addFileToList(files, filesToAdd));
-  };
-
-  function handleOnDragEnd(result: DropResult) {
-    if (!files) return;
-    setFiles(sortFileList(files, result));
-  }
-
-  // manage controlled form state
-  const handleFormChange = (
-    e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
-  ) => {
-    setFormData(
-      handleFormInputs(e, formData) as SetStateAction<ListingDataForm>
-    );
-  };
-
-  // submit form
-  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
-    setError("");
-    e.preventDefault();
-    if (!files?.length) {
-      setError("please upload at least 1 image");
-      return;
-    }
-    const { error, validation } = validateCreateListingForm(formData, files);
-    if (!validation) {
-      setError(error);
-      return;
-    }
-    setLoading(true);
-
+  const handleDeleteAccount = async () => {
     refreshToken().then(async (result) => {
       if (!result?.isAuthenticated) {
-        setLoading(false);
         return;
       }
-      const images: string[] = [];
-      for (let i = 0; i < files.length; i++) {
-        await uploadImage(files[i])
-          .then((result) => {
-            if (result.downloadURL) {
-              images.push(result.downloadURL as string);
-            } else {
-              const errorMessage = result.error
-                ? result.error.message
-                : "Unknown error";
-              setFileUploadError(errorMessage);
-            }
-          })
-          .catch(() => {
-            setFileUploadError("Unexpected error occurred");
-          });
-      }
       try {
-        setError("");
-        const res = await fetch(`/api/property/create`, {
-          method: "POST",
-          credentials: "include",
-          headers: {
-            "Content-Type": "application/json",
-            authorization: `Bearer ${result.accessToken}`,
-          },
-          body: JSON.stringify({
-            ...formData,
-            imageUrls: images,
-          }),
-        });
+        const res = await fetch(
+          `/api/user/delete/${
+            currentUser?._id
+          }`,
+          {
+            credentials: "include",
+            method: "DELETE",
+            headers: {
+              "Content-Type": "application/json",
+              authorization: `Bearer ${result.accessToken}`,
+            },
+          }
+        );
         const data = await res.json();
-        setLoading(false);
-        setFiles(undefined);
-        setFormData(initialListingDataForm);
         if (!data.success) {
           setError(data.message);
+          return;
         }
-        dispatch(setUser(data.user));
-        navigate(`/listing/${data.property._id}`);
+        dispatch(signOut());
+        toast.success("Profile has been deleted");
       } catch (error) {
-        setLoading(false);
         if (error instanceof Error) {
           setError(error.message);
         }
       }
     });
   };
-  return {
-    handleSubmit,
-    removeFile,
-    handleFormChange,
-    addFiles,
-    handleOnDragEnd,
-    fileUploadError,
-    formData,
-    loading,
-    files,
-    error,
+
+  const signOutUser = async () => {
+    try {
+      const res = await fetch(
+        `/api/auth/signout`,
+        { credentials: "include" }
+      );
+      const data = await res.json();
+      if (!data.success) {
+        setError(data.message);
+        return;
+      }
+      dispatch(signOut());
+    } catch (error) {
+      if (error instanceof Error) {
+        setError(error.message);
+      }
+    }
   };
+  return { handleDeleteAccount, error, signOutUser };
 }
-export default useCreateListing;
+
+export default useProfileActions;
